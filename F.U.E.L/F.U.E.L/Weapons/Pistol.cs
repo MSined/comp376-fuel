e3f6c592fe3c1ae24dc6bf62c4a04ecfa3421146
pm.deblois <pm.deblois@b7754fb0-d8dc-5596-4ff7-25ba76912955>
2011-11-02 17:45:25 +0000

    Created a few weapons and an AOEBullet (waiting on collision detection to implement the damage in a radius)

---
+++ F.U.E.L/F.U.E.L/Weapons/Pistol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace F.U.E.L
+{
+    class Pistol : Weapon
+    {
+        private const int RANGE = 30;
+        private const int DAMAGE = 10;
+        private const int FIRERATE = 10000000;
+
+        public Pistol(Game game, Model[] modelComponents, Vector3 position/*,
+            ALREADY SET -> int range, int damage, int fireRate*/)
+            : base(game, modelComponents, position, RANGE, DAMAGE, FIRERATE)
+        {
+            
+        }
+
+        public override void Draw(Camera camera)
+        {
+
+        }
+    }
+}
