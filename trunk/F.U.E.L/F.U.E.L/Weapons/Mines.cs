5734ebf63cc04ec7cf813bee187bbf79cc7ea378
pm.deblois <pm.deblois@b7754fb0-d8dc-5596-4ff7-25ba76912955>
2011-11-02 17:45:25 +0000

    Created a few weapons and an AOEBullet (waiting on collision detection to implement the damage in a radius)

---
+++ trunk/F.U.E.L/F.U.E.L/Weapons/Mines.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace F.U.E.L
+{
+    class Mine : Weapon
+    {
+        private const int RANGE = 10;
+        private const int DAMAGE = 100;
+        private const int FIRERATE = 10000000;
+
+        public Mine(Game game, Model[] modelComponents, Vector3 position/*,
+            ALREADY SET -> int range, int damage, int fireRate*/)
+            : base(game, modelComponents, position, RANGE, DAMAGE, FIRERATE)
+        {
+            
+        }
+
+        public override void shoot(Vector3 position, Vector3 direction)
+        {
+            long nowTick = DateTime.Now.Ticks;
+
+            if (lastShot + fireRate < nowTick)
+            {
+                game.Components.Add(new AOEBullet(game, this.bulletModelComponents, position, new Vector3(0,0,0), range, damage));
+                lastShot = nowTick;
+            }
+        }
+
+        public override void Draw(Camera camera)
+        {
+
+        }
+    }
+}
