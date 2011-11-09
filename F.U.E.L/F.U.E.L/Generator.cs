6d4ee6dbb90bdbc810f0cb0de1736a2d75a82b51
yipksj <yipksj@b7754fb0-d8dc-5596-4ff7-25ba76912955>
2011-11-09 21:37:12 +0000

    worked on the Generator, added a bool and an int in Player for repairing generator. changed the player in Game1 to a List of players

--- F.U.E.L/F.U.E.L/Generator.cs
+++ F.U.E.L/F.U.E.L/Generator.cs
@@ -10,12 +10,44 @@ namespace F.U.E.L
 {
     class Generator : Building
     {
+        private int topHP;
+        private int hp;
+        private bool functional;
+        public float repairRange;
+        public List<Player> players;
+ 
         public Generator(Game game, Model[] modelComponents, Vector3 position,
-            float angle)
+            float angle,
+            int topHP, int hp, bool functional, float repairRange, List<Player> players)
             : base(game, modelComponents, position, angle)
         {
-
+            this.topHP = topHP;
+            this.hp = hp;
+            this.functional = functional;
+            this.repairRange = repairRange;
+            this.players = players;
         }
 
+        private void repair(List<Player> players)
+        {
+            foreach (Player p in players) {
+                if ((p.position - this.position).Length() <= repairRange && p.repairing) {
+                    hp += p.repairSpeed;
+                    if (hp >= topHP) {
+                        hp = topHP;
+                        functional = true;
+                    }
+                }
+            }
+        }
+
+        public override void Update(GameTime gameTime, List<Object> colliders)
+        {
+            repair(players);
+            if (hp <= 0) {
+                hp = 0;
+                functional = false;
+            }
+        }
     }
 }
